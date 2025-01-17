fbd6ff13109db6a05035cfc7caac1c7a75c81053
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-01-18 07:33:35 +0800

    Hide "Go to Result Table" button if control panel display isn't block

--- src/components/panel/ControlPanelGoToResultTableButton.tsx
+++ src/components/panel/ControlPanelGoToResultTableButton.tsx
@@ -1,15 +1,18 @@
-import { SchedulerResultContext } from "@hooks/index";
+import { SchedulerResultContext, useWindowSize } from "@hooks/index";
 import { useContext } from "react";
 
 export default function ControlPanelGoToResultTableButton() {
     const { state: schedulerResult } = useContext(SchedulerResultContext);
+    const [width] = useWindowSize();
 
     return (
         <button
             type="button"
             id="go-to-result-table"
             hidden={
-                !schedulerResult || schedulerResult.ganttChartData.length === 0
+                width >= 1000 ||
+                !schedulerResult ||
+                schedulerResult.ganttChartData.length === 0
             }
             onClick={() => {
                 window.location.hash = "result-table";
