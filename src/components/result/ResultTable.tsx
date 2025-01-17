fbb50ebc7da186ec004b0c6fe26e48c256068971
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-01-18 07:17:28 +0800

    Put result table in a container for horizontal overflow scroll

--- src/components/result/ResultTable.tsx
+++ src/components/result/ResultTable.tsx
@@ -10,35 +10,37 @@ export default function ResultTable() {
     }
 
     return (
-        <table id="result-table" className="result-table">
-            <thead>
-                <tr>
-                    <th style={{ maxWidth: "30%" }}>Process</th>
-                    <th>Arrival Time</th>
-                    <th>Burst Time</th>
-                    <th>Waiting Time</th>
-                    <th>Turnaround Time</th>
-                    <th>Exit Time</th>
-                    <th>Response Time</th>
-                </tr>
-            </thead>
-            <tbody>
-                {Object.keys(schedulerResult.waitingTime).map((key, i) => {
-                    const process = schedulerResult.processes[key];
+        <div className="result-table-container">
+            <table id="result-table" className="result-table">
+                <thead>
+                    <tr>
+                        <th style={{ maxWidth: "30%" }}>Process</th>
+                        <th>Arrival Time</th>
+                        <th>Burst Time</th>
+                        <th>Waiting Time</th>
+                        <th>Turnaround Time</th>
+                        <th>Exit Time</th>
+                        <th>Response Time</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {Object.keys(schedulerResult.waitingTime).map((key, i) => {
+                        const process = schedulerResult.processes[key];
 
-                    return (
-                        <tr key={`${key}-${i.toString()}`}>
-                            <td>{key}</td>
-                            <td>{process.arrivalTime}</td>
-                            <td>{process.burstTime}</td>
-                            <td>{schedulerResult.waitingTime[key]}</td>
-                            <td>{schedulerResult.turnaroundTime[key]}</td>
-                            <td>{schedulerResult.exitTime[key]}</td>
-                            <td>{schedulerResult.responseTime[key]}</td>
-                        </tr>
-                    );
-                })}
-            </tbody>
-        </table>
+                        return (
+                            <tr key={`${key}-${i.toString()}`}>
+                                <td>{key}</td>
+                                <td>{process.arrivalTime}</td>
+                                <td>{process.burstTime}</td>
+                                <td>{schedulerResult.waitingTime[key]}</td>
+                                <td>{schedulerResult.turnaroundTime[key]}</td>
+                                <td>{schedulerResult.exitTime[key]}</td>
+                                <td>{schedulerResult.responseTime[key]}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
     );
 }
